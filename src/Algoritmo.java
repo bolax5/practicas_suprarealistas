4eec59e116f55dd95ba56fbc0d6ea766c24184b7
jorgeearanda2 <jorgeearanda2@gmail.com>
2017-10-05 20:05:27 +0200

    Proyecto

---
+++ src/Algoritmo.java
@@ -0,0 +1,24 @@
+
+public class Algoritmo {
+	public boolean[] seleccionActividades(int[] c, int [] f)
+    
+    {
+        boolean [] sol = new boolean [c.length];
+        sol[0] = true;
+        int i = 0;
+        for (int j = 1; j<c.length; j++){
+            if (c[j]>=f[i]){
+                sol[j] = true;
+                i=j;
+            }else{
+                sol[j] = false;
+            }    
+        }
+        for (int k = 0; k<c.length; k++){
+            System.out.println(sol[k]);
+        }
+        return sol;
+    }
+
+
+}
