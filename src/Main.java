191d06682d7e5506bc8cf44cc9f6d27e5a164119
jorgeearanda2 <jorgeearanda2@gmail.com>
2017-10-06 12:27:29 +0200

    ProyectoAct1

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,10 @@
 public class Main {
 
 	public static void main(String[] args) {
-		 int [] f = {3,8,11,18,20,21,24,24,25,29};
-		 int [] c = {0,7,5,2,16,11,23,15,12,14};
+		 int [] fin = {3,8,11,18,20,21,24,24,25,29};
+		 int [] com = {0,7,5,2,16,11,23,15,12,24};
 		 Algoritmo alg = new Algoritmo();
-		 alg.seleccionActividades(c, f);
+		 alg.seleccionActividades(com, fin);
 
 	}
 
