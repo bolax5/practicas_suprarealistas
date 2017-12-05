d7e353a412560142152bd0ae6bf7d542d43362d6
jorgeearanda2 <jorgeearanda2@gmail.com>
2017-12-05 17:43:08 +0100

    Finikitao

--- Practica4/src/ProgramacionDinamica.java
+++ Practica4/src/ProgramacionDinamica.java
@@ -2,16 +2,30 @@
 public class ProgramacionDinamica {
 
 	public double beneficio (int c_inicial, int[]GCD, double[] RCD, int [] GBT, double [] RBT ){
-		double inversiones[] = new double [GCD.length];
+		double inversiones[] = new double [GCD.length+1];
 		inversiones[0] = c_inicial;
 		for (int i = 1; i<=Math.min(5,GCD.length-1);i++){
 			inversiones[i] = Math.max(inversiones[i-1],(inversiones[i-1]-GCD[i-1])*RCD[i-1]);
 			System.out.println("En el mes " + i + " : " + inversiones[i]);
 		}
-		for(int i = 6; i< GCD.length;i++){
+		for(int i = 6; i<= GCD.length;i++){
 			inversiones[i] = Math.max(inversiones[i-1],Math.max((inversiones[i-1]-GCD[i-1])*RCD[i-1],(inversiones[i-6]-GBT[i-6])*RBT[i-6]));
 			System.out.println("En el mes " +i+ " : " + inversiones[i]);
 		}
-		return inversiones[GCD.length-1];
+		return inversiones[GCD.length];
 	}
+	public double[] beneficioSolucion (int c_inicial, int[]GCD, double[] RCD, int [] GBT, double [] RBT ){
+		double inversiones[] = new double [GCD.length+1];
+		inversiones[0] = c_inicial;
+		for (int i = 1; i<=Math.min(5,GCD.length-1);i++){
+			inversiones[i] = Math.max(inversiones[i-1],(inversiones[i-1]-GCD[i-1])*RCD[i-1]);
+			System.out.println("En el mes " + i + " : " + inversiones[i]);
+		}
+		for(int i = 6; i<= GCD.length;i++){
+			inversiones[i] = Math.max(inversiones[i-1],Math.max((inversiones[i-1]-GCD[i-1])*RCD[i-1],(inversiones[i-6]-GBT[i-6])*RBT[i-6]));
+			System.out.println("En el mes " +i+ " : " + inversiones[i]);
+		}
+		return inversiones;
+	}
+	
 }
--- Practica4/src/main.java
+++ Practica4/src/main.java
@@ -3,14 +3,16 @@ public class main {
 
 	public static void main(String[] args) {
 		EliminacionRecursividad er = new EliminacionRecursividad();
-		int a = er.tabOpt(6, 2);
+		int a = er.tabOpt(4, 2);
 		System.out.println(a);
+		
 		int GCD[] = {200,50,100,75,100,50,80};
 		double RCD[] = {1.01,1.02,1.1,1.03,1.1,1.2,1.06};
 		int GBT[] = {75,100,200,50,200,50,75};
-		double RBT[] = {1.03,1.03,1.3,1.2,1.02,1.2,1.05};
+		double RBT[] = {1.03,1.5,1.3,1.2,1.02,1.2,1.05};
 		ProgramacionDinamica pd = new ProgramacionDinamica();
 		double d = pd.beneficio(4000, GCD, RCD, GBT, RBT);
+		pd.beneficioSolucion(4000, GCD, RCD, GBT, RBT);
 		System.out.println(d);
 	}
 
