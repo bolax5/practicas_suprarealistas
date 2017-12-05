527eef3c69edfcac90c083d6ecdaec0768b45b6f
jorgeearanda2 <jorgeearanda2@gmail.com>
2017-12-05 20:20:55 +0100

    Fin 5 diciembre

--- Practica4/src/ProgramacionDinamica.java
+++ Practica4/src/ProgramacionDinamica.java
@@ -2,30 +2,26 @@
 public class ProgramacionDinamica {
 
 	public double beneficio (int c_inicial, int[]GCD, double[] RCD, int [] GBT, double [] RBT ){
-		double inversiones[] = new double [GCD.length+1];
-		inversiones[0] = c_inicial;
+		double beneficios[] = new double [GCD.length+1];
+		beneficios[0] = c_inicial;
 		for (int i = 1; i<=Math.min(5,GCD.length-1);i++){
-			inversiones[i] = Math.max(inversiones[i-1],(inversiones[i-1]-GCD[i-1])*RCD[i-1]);
-			System.out.println("En el mes " + i + " : " + inversiones[i]);
+			beneficios[i] = Math.max(beneficios[i-1],(beneficios[i-1]-GCD[i-1])*RCD[i-1]);
 		}
 		for(int i = 6; i<= GCD.length;i++){
-			inversiones[i] = Math.max(inversiones[i-1],Math.max((inversiones[i-1]-GCD[i-1])*RCD[i-1],(inversiones[i-6]-GBT[i-6])*RBT[i-6]));
-			System.out.println("En el mes " +i+ " : " + inversiones[i]);
+			beneficios[i] = Math.max(beneficios[i-1],Math.max((beneficios[i-1]-GCD[i-1])*RCD[i-1],(beneficios[i-6]-GBT[i-6])*RBT[i-6]));
 		}
-		return inversiones[GCD.length];
-	}
+		return beneficios[GCD.length];
+	} 
 	public double[] beneficioSolucion (int c_inicial, int[]GCD, double[] RCD, int [] GBT, double [] RBT ){
-		double inversiones[] = new double [GCD.length+1];
-		inversiones[0] = c_inicial;
+		double beneficios[] = new double [GCD.length+1];
+		beneficios[0] = c_inicial;
 		for (int i = 1; i<=Math.min(5,GCD.length-1);i++){
-			inversiones[i] = Math.max(inversiones[i-1],(inversiones[i-1]-GCD[i-1])*RCD[i-1]);
-			System.out.println("En el mes " + i + " : " + inversiones[i]);
+			beneficios[i] = Math.max(beneficios[i-1],(beneficios[i-1]-GCD[i-1])*RCD[i-1]);
 		}
 		for(int i = 6; i<= GCD.length;i++){
-			inversiones[i] = Math.max(inversiones[i-1],Math.max((inversiones[i-1]-GCD[i-1])*RCD[i-1],(inversiones[i-6]-GBT[i-6])*RBT[i-6]));
-			System.out.println("En el mes " +i+ " : " + inversiones[i]);
+			beneficios[i] = Math.max(beneficios[i-1],Math.max((beneficios[i-1]-GCD[i-1])*RCD[i-1],(beneficios[i-6]-GBT[i-6])*RBT[i-6]));
 		}
-		return inversiones;
+		return beneficios;
 	}
 	
 }
