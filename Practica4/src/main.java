2e65910eb3416f4bf3c7a7027e3883305816e98b
jorgeearanda2 <jorgeearanda2@gmail.com>
2017-12-06 11:30:07 +0100

    Main cambiao

--- Practica4/src/main.java
+++ Practica4/src/main.java
@@ -2,18 +2,24 @@
 public class main {
 
 	public static void main(String[] args) {
-		EliminacionRecursividad er = new EliminacionRecursividad();
-		int a = er.tabOpt(4, 2);
-		System.out.println(a);
+		
+		int c = EliminacionRecursividad.mem(4, 2);
+		System.out.println("El resultado para el algoritmo de memorizacion es : " + c);
+		c = EliminacionRecursividad.tab(4, 2);
+		System.out.println("El resultado para el algoritmo de tabulacion es : " + c);
+		c = EliminacionRecursividad.tabOpt(4, 2);
+		System.out.println("El resultado para el algoritmo de tabulacion optimizada es : " + c);
+		
 		
 		int GCD[] = {200,50,100,75,100,50,80};
 		double RCD[] = {1.01,1.02,1.1,1.03,1.1,1.2,1.06};
 		int GBT[] = {75,100,200,50,200,50,75};
 		double RBT[] = {1.03,1.5,1.3,1.2,1.02,1.2,1.05};
+		
 		ProgramacionDinamica pd = new ProgramacionDinamica();
 		double d = pd.beneficio(4000, GCD, RCD, GBT, RBT);
 		pd.beneficioSolucion(4000, GCD, RCD, GBT, RBT);
-		System.out.println(d);
+		System.out.println("El beneficio obtenido es : " + d);
 	}
 
 }
