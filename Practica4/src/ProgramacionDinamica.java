670174f0dceba7c3a893d3579d8ecf6e6beb6418
Jose Vicente Bañuls García <banulsgarcia.josevicente@gmail.com>
2017-12-06 11:29:38 +0100

    decreased memory consumption by 8 bits

--- Practica4/src/ProgramacionDinamica.java
+++ Practica4/src/ProgramacionDinamica.java
@@ -7,11 +7,12 @@ public class ProgramacionDinamica {
 	} 
 	public double[] beneficioSolucion (int c_inicial, int[]GCD, double[] RCD, int [] GBT, double [] RBT ){
 		double beneficios[] = new double [GCD.length+1];
+		int i = 0;
 		beneficios[0] = c_inicial;
-		for (int i = 1; i<=Math.min(5,GCD.length-1);i++){
+		for (i = 1; i<=Math.min(5,GCD.length-1);i++){
 			beneficios[i] = Math.max(beneficios[i-1],(beneficios[i-1]-GCD[i-1])*RCD[i-1]);
 		}
-		for(int i = 6; i<= GCD.length;i++){
+		for(i = 6; i<= GCD.length;i++){
 			beneficios[i] = Math.max(beneficios[i-1],Math.max((beneficios[i-1]-GCD[i-1])*RCD[i-1],(beneficios[i-6]-GBT[i-6])*RBT[i-6]));
 		}
 		return beneficios;
