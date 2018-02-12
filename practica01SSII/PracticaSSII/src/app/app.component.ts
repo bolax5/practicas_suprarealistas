f64dc3ae2baf384e0cbc7211868bc41cb17e8ca0
jorgeearanda2 <jorgeearanda2@gmail.com>
2018-02-12 20:19:39 +0100

    added images an descriptions

--- practica01SSII/PracticaSSII/src/app/app.component.ts
+++ practica01SSII/PracticaSSII/src/app/app.component.ts
@@ -38,86 +38,103 @@ export class AppComponent implements OnInit {
     {
       title: 'Monster Hunter World',
       description: 'Empuña de nuevo tu Gran Espada y adéntrate en el "Nuevo Mundo" para dar caza a los monstruos más feroces jamás conocidos. Tras 9 años desde su última aparición en consolas de sobremesa, la saga Monster Hunter, producida por Ryozo Tsujimoto, regresa con el título Monster Hunter World, la nueva entrega de caza lanzada de manera simultánea en todo el mundo',
-      imgSource : '../assets/img/MonsterHunterWorld.png'
+      imgSource: '../assets/img/MonsterHunterWorld.png'
     },
     {
       title: 'The Last Of Us',
-      description: 'descript',
-      imgSource : '../assets/img/Last.jpg'
+      description: 'En un mundo hostil y post-pandémico, las circunstancias desesperadas acaban uniendo a Ellie y Joel, quienes deben confiar el uno en el otro para sobrevivir en un viaje salvaje por lo que queda de Estados Unidos.',
+      imgSource: '../assets/img/Last.jpg'
     },
     {
       title: 'The Binding of Isaac: Afterbirth ',
-      description: 'descript',
+      description: 'The Binding of Isaac: Afterbirth+ es un juego de acción en vista cenital, con grandes dosis de elementos RPG y Rogue-like en el que los mundos se generan de manera aleatoria, dando lugar a probabilidades casi infinitas',
       imgSource: '../assets/img/Isaac.jpg'
     },
     {
       title: 'Los Sims 4',
-      description: 'descript',
+      description: 'Los Sims 4 es un videojuego de simulación social y de vida',
       imgSource: '../assets/img/Sims.jpg'
     },
     {
-      title: 'tercer',
-      description: 'descript'
+      title: 'Ratchet & Clank',
+      description: '¡El remake basado en la película que está basada en el juego! Únete a Ratchet y Clank y soporta el ego del Capitán Qwark en una aventura intergaláctica para salvar la galaxia de Solana.',
+      imgSource: '../assets/img/Ratchet.jpg'
     },
     {
-      title: 'primerisimo',
-      description: 'descript'
+      title: 'Rocket League',
+      description: 'Rocket League es un videojuego que combina el fútbol con los vehículos.',
+      imgSource: '../assets/img/Rocket.jpg'
     },
     {
-      title: 'segundo',
-      description: 'descript'
+      title: 'Mario + Rabbids Kingdom Battle',
+      description: 'El universo de Super Mario es invadido por los Rabbids en Mario + Rabbids Kingdom Battle!',
+      imgSource: '../assets/img/Mario.jpg'
     },
     {
-      title: 'tercer',
-      description: 'descript'
+      title: 'Kingdom Hearts III',
+      description: 'Sora, Donald, y Goofy vuelven en una aventura cargada de magia y emoción con Kingdom Hearts III',
+      imgSource: '../assets/img/Kingdom.png'
     },
     {
-      title: 'primerisimo',
-      description: 'descript'
+      title: 'Assassin\'s Creed Origins',
+      description: 'Cuando todo termina, hay un comienzo. Egipto está al borde del colapso. Poderosos monarcas luchan entre sí para disputarse el reino, mientras que otras figuras ocultas acechan entre las sombras listas para hacerse el control.',
+      imgSource: '../assets/img/Assassin.jpg'
     },
     {
-      title: 'segundo',
-      description: 'descript'
+      title: 'Counter Strike Global Offensive',
+      description: 'Counter-Strike: Global Offensive es un videojuego de disparos en primera persona desarrollado por Valve Corporation en cooperación con Hidden Path Entertainment, y es el cuarto juego de la saga Counter-Strike',
+      imgSource: '../assets/img/CSGO.jpg'
     },
     {
-      title: 'tercer',
-      description: 'descript'
+      title: 'Gran Turismo Sport',
+      description: 'Pisa el acelerador para disfrutar del simulador de carreras más auténtico y espectacular que has visto jamás: GT Sport',
+      imgSource: '../assets/img/gt.jpg'
     },
     {
-      title: 'primerisimo',
-      description: 'descript'
+      title: 'Minecraft',
+      description: 'Minecraft es un juego de construcción, aventura y supervivencia. Su gran libertad de acción y la posibilidad de personalizarlo con pieles y mods dotan a este juego de una vida casi infinita',
+      imgSource: '../assets/img/minecraft.jpg'
     },
     {
-      title: 'segundo',
-      description: 'descript'
+      title: 'Shadow of the colossus',
+      description: 'En Shadow of the Colossus encarnaremos una vez más a Wander, siendo nuestra misión encontrar a los 16 colosos de piedra repartidos por el mundo para salvar a mono de su interminable sueño',
+      imgSource: '../assets/img/Shadow.png'
     },
     {
-      title: 'tercer',
-      description: 'descript'
+      title: 'Horizon: Zero Dawn',
+      description: 'Repudiada por su tribu al nacer, Aloy ha pasado toda su vida buscando respuestas a antiguos secretos y preguntas prohibidas. En una época en la que la humanidad ya no es la especie dominante y seres mecánicos con forma animal nos han arrebatado el control, la joven cazadora se embarcará en un viaje a través de un gigantesco mundo abierto post-apocalíptico para descubrir por qué la Tierra ya no nos pertenece',
+      imgSource: '../assets/img/Horizon.jpg'
     },
     {
-      title: 'primerisimo',
-      description: 'descript'
+      title: 'La Tierra Media: Sombras de Guerra',
+      description: 'El señor oscuro ha regresado a la Tierra Media para reunir a sus ejércitos y gobernarlos a todos. La historia de Talion y Celebrimbor no ha terminado todavía ya que el conflicto está próximo, una guerra por Mordor ha comenzado.',
+      imgSource: '../assets/img/Sombras.jpg'
+
     },
     {
-      title: 'segundo',
-      description: 'descript'
+      title: 'Resident Evil 6',
+      description: 'El imparable Virus-C se ha desencadenado en un mundo desprevenido y desde Norteamérica a Europa del Este y China se están produciendo horribles mutaciones mortales.',
+      imgSource: '../assets/img/resident.jpg'
     },
     {
-      title: 'tercer',
-      description: 'descript'
+      title: 'The Legend of Zelda : Breath of the Wild',
+      description: 'The Legend of Zelda: Breath of the Wild te sumergirá en un mundo de descubrimiento con un impresionante estilo artístico similar a The Wind Waker o Skyward Sword, una cautivadora banda sonora y una intrigante y melancólica historia.',
+      imgSource: '../assets/img/Zelda.jpg'
     },
     {
-      title: 'primerisimo',
-      description: 'descript'
+      title: 'Crash Bandicoot N.Sane Trilogy',
+      description: 'Acompaña de nuevo a Crash saltando, girando y comiendo Frutas Wumpa mientras haces frente a épicos desafíos y aventuras a través de los tres juegos de plataformas que iniciaron la saga, ahora remasterizados en alta definición',
+      imgSource: '../assets/img/crash.jpg'
     },
     {
-      title: 'segundo',
-      description: 'descript'
+      title: 'Star Wars Battlefront II',
+      description: 'Corre al frente de la batalla en la esperada secuela del título de acción multijugador de DICE basado en la Guerra de las Galaxias.',
+      imgSource: '../assets/img/StarWars.jpg'
     },
     {
-      title: 'tercer',
-      description: 'descript'
+      title: 'Just Dance 2018',
+      description: 'No importa de dónde eres, qué edad tienes, si eres un panda o una zanahoria... ¡Sube el volumen y saca al bailarín que llevas dentro porque Just Dance 2018 hace bailar a todo el mundo!',
+      imgSource: '../assets/img/dance.jpg'
     }];
 
 
