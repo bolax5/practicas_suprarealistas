9201d6eabfb2ffb3d6d864777f1b31921a997c94
Jose Vicente Bañuls García <banulsgarcia.josevicente@gmail.com>
2018-03-06 17:06:50 +0100

    hey

--- practica01SSII/PracticaSSII/src/app/services/login.service.ts
+++ practica01SSII/PracticaSSII/src/app/services/login.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable()
 export class LoginService {
@@ -8,7 +8,10 @@ export class LoginService {
 
   sendLogin(login: {username: string, password: string}) {
     // Faltaría pasarle el id a la petición para que funcione
-    return this.http.post<any>('http://localhost:8080/login', login);
+    const header = new HttpHeaders({
+      'Content-Type': 'application/json'
+    });
+    return this.http.post<any>('http://localhost:8080/login', login, {headers: header});
   }
 
 }
